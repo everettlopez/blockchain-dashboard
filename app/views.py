e170117062898ea262a0463fc459ffdb0b739271
Everett Lopez <everettlopezsr@gmail.com>
2026-03-01 19:02:20 -0700

    Created a user in the Django DB to test the login workflow. Needs to handle error exceptions better.

--- app/views.py
+++ app/views.py
@@ -1,21 +1,45 @@
 from django.shortcuts import render
+
 from app.ingestion.cmc import fetch_prices
 from app.ingestion.alchemy import fetch_tokens_by_wallet
+from app.forms import LoginForm
 
-
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate, login 
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     tokens = ["BTC", "ETH", "LINK", "DOT"]
     token_prices = fetch_prices(tokens)
 
+    
+
     return render(request, "index.html", {
         "token_prices": token_prices
     })
 
 def signup(request):
-    return render(request, "login.html")
+    form = LoginForm(request.POST or None)
 
+    if request.method == "POST" and form.is_valid():
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
+        else:
+            form.add_error(None, "Invalid username or password")
+
+    return render(request, "login.html", {
+        "form": form
+    })
+
+@login_required
 def dashboard(request):
 
     wallet_address = "0x1E6E8695FAb3Eb382534915eA8d7Cc1D1994B152"
